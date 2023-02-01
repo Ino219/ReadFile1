61dab28ad31d8efe8da3f13cff8d3be486b9232b
Ino <ainstain1220@gmail.com>
2023-02-02 02:18:31 +0900

    addxml

--- ReadFile1/MyForm.h
+++ ReadFile1/MyForm.h
@@ -19,7 +19,8 @@ namespace ReadFile1 {
 
 	using namespace System::IO;
 	using namespace System::IO::Compression;
-	
+	using namespace System::Xml;
+
 
 	/// <summary>
 	/// MyForm の概要
@@ -51,6 +52,7 @@ namespace ReadFile1 {
 	private: System::Windows::Forms::Button^  button2;
 	private: System::Windows::Forms::Button^  button3;
 	private: System::Windows::Forms::Button^  button4;
+	private: System::Windows::Forms::Button^  button5;
 	protected:
 
 	private:
@@ -71,6 +73,7 @@ namespace ReadFile1 {
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->button3 = (gcnew System::Windows::Forms::Button());
 			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->button5 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// textBox1
@@ -124,12 +127,23 @@ namespace ReadFile1 {
 			this->button4->UseVisualStyleBackColor = true;
 			this->button4->Click += gcnew System::EventHandler(this, &MyForm::button4_Click);
 			// 
+			// button5
+			// 
+			this->button5->Location = System::Drawing::Point(169, 168);
+			this->button5->Name = L"button5";
+			this->button5->Size = System::Drawing::Size(75, 23);
+			this->button5->TabIndex = 5;
+			this->button5->Text = L"button5";
+			this->button5->UseVisualStyleBackColor = true;
+			this->button5->Click += gcnew System::EventHandler(this, &MyForm::button5_Click);
+			// 
 			// MyForm
 			// 
 			this->AllowDrop = true;
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 12);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(284, 261);
+			this->Controls->Add(this->button5);
 			this->Controls->Add(this->button4);
 			this->Controls->Add(this->button3);
 			this->Controls->Add(this->button2);
@@ -152,15 +166,15 @@ namespace ReadFile1 {
 		//ディレクトリ名取得
 		String^ directory = System::IO::Path::GetDirectoryName(file[0]);
 		//interopの宣言
-		Microsoft::Office::Interop::Excel::Application^ app_=nullptr;
-		Microsoft::Office::Interop::Excel::Workbook^ workbook=nullptr;
-		Microsoft::Office::Interop::Excel::Worksheet^ worksheet=nullptr;
-		Microsoft::Office::Interop::Excel::Range^ samRange=nullptr;
-		Microsoft::Office::Interop::Excel::Range^ workcells=nullptr;
-		Microsoft::Office::Interop::Excel::Range^ allcells=nullptr;
+		Microsoft::Office::Interop::Excel::Application^ app_ = nullptr;
+		Microsoft::Office::Interop::Excel::Workbook^ workbook = nullptr;
+		Microsoft::Office::Interop::Excel::Worksheet^ worksheet = nullptr;
+		Microsoft::Office::Interop::Excel::Range^ samRange = nullptr;
+		Microsoft::Office::Interop::Excel::Range^ workcells = nullptr;
+		Microsoft::Office::Interop::Excel::Range^ allcells = nullptr;
 		Microsoft::Office::Interop::Excel::Range^ targetCell = nullptr;
 
-		
+
 
 		//MessageBox::Show(file[0]);
 		//MessageBox::Show(title);
@@ -168,7 +182,7 @@ namespace ReadFile1 {
 		//MessageBox::Show(directory);
 		//extension==".xlsx","xls","xlw"という記述だとコードは通るが、if文がきかなくなる。
 		//例えば、指定したファイル形式でなくても、if文を通るようになってしまう
-		if (extension == ".xlsx"|| extension=="xls"||extension== "xlw") {
+		if (extension == ".xlsx" || extension == "xls" || extension == "xlw") {
 			try {
 				app_ = gcnew Microsoft::Office::Interop::Excel::ApplicationClass();
 				//Excelブックの表示はしない
@@ -192,7 +206,7 @@ namespace ReadFile1 {
 					Type::Missing));
 				//一枚目のワークシートを開く
 				worksheet = (Microsoft::Office::Interop::Excel::Worksheet^)workbook->Worksheets[1];
-				
+
 				allcells = (Microsoft::Office::Interop::Excel::Range^)worksheet->Cells;
 				//worksheet->Cells[1,1]の記述にはworksheet->Cells(全セル)と[1,1]の両方のrangeを含むので、allcells変数を作成し、後で解放する必要がある
 				workcells = (Microsoft::Office::Interop::Excel::Range^)worksheet->Cells[1, 1];
@@ -239,7 +253,7 @@ namespace ReadFile1 {
 					MessageBox::Show("結合セルです:"+workcells->Text->ToString());
 				}
 
-				
+
 				String^ target = "ターゲット";
 				samRange = nullptr;
 				//allcellsをrange範囲に指定しないと、findは回ってもfindnextが回らない
@@ -273,24 +287,24 @@ namespace ReadFile1 {
 				}*/
 				while (true) {
 					//処理を記述
-					Microsoft::Office::Interop::Excel::WorksheetFunction^ works = app_->WorksheetFunction; 
+					Microsoft::Office::Interop::Excel::WorksheetFunction^ works = app_->WorksheetFunction;
 					try {
-						
+
 						works->Match(samcell, allcells, 0);
 					}
 					catch (Exception^ e) {
 						MessageBox::Show(e->ToString());
 					}
 					finally{
-				    System::Runtime::InteropServices::Marshal::ReleaseComObject(works);
+					System::Runtime::InteropServices::Marshal::ReleaseComObject(works);
 					}
 					break;
 					//samRange = worksheet->Cells(WorksheetFunction::Match(pad, allcells, 0), 1);
 				}
 
 
-				
-				
+
+
 			}
 			catch (Exception^ e) {
 				MessageBox::Show(e->ToString());
@@ -299,42 +313,42 @@ namespace ReadFile1 {
 				//COM解放
 				app_->Workbooks->Close();
 
-				//System::Runtime::InteropServices::Marshal::ReleaseComObject(targetCell);
-				//System::Runtime::InteropServices::Marshal::ReleaseComObject(samRange);
-				
-				System::Runtime::InteropServices::Marshal::ReleaseComObject(workcells);
-				System::Runtime::InteropServices::Marshal::ReleaseComObject(allcells);
-				System::Runtime::InteropServices::Marshal::ReleaseComObject(worksheet);
-				System::Runtime::InteropServices::Marshal::ReleaseComObject(workbook);
-				System::Runtime::InteropServices::Marshal::ReleaseComObject(app_);
+			//System::Runtime::InteropServices::Marshal::ReleaseComObject(targetCell);
+			//System::Runtime::InteropServices::Marshal::ReleaseComObject(samRange);
+
+			System::Runtime::InteropServices::Marshal::ReleaseComObject(workcells);
+			System::Runtime::InteropServices::Marshal::ReleaseComObject(allcells);
+			System::Runtime::InteropServices::Marshal::ReleaseComObject(worksheet);
+			System::Runtime::InteropServices::Marshal::ReleaseComObject(workbook);
+			System::Runtime::InteropServices::Marshal::ReleaseComObject(app_);
 
 			}
 		}
-		else if (extension == "txt",".txt") {
-		StreamReader^ sr = gcnew StreamReader(file[0]);
-		String^ line;
-		System::Collections::Generic::List<String^> strList;
-		cli::array<String^>^ arrays;
-		try {
-			while ((line = sr->ReadLine()) != nullptr) {
-				if (line->Contains("VVV")) {
-					//ラベルの取得.arrayで受ける
-					arrays=line->Split(' ');
+		else if (extension == "txt", ".txt") {
+			StreamReader^ sr = gcnew StreamReader(file[0]);
+			String^ line;
+			System::Collections::Generic::List<String^> strList;
+			cli::array<String^>^ arrays;
+			try {
+				while ((line = sr->ReadLine()) != nullptr) {
+					if (line->Contains("VVV")) {
+						//ラベルの取得.arrayで受ける
+						arrays = line->Split(' ');
+					}
+				}
+				for each (String^ var in arrays)
+				{
+					MessageBox::Show(var);
 				}
 			}
-			for each (String^ var in arrays)
-			{
-				MessageBox::Show(var);
+			catch (Exception^ e) {
+				MessageBox::Show(e->ToString());
+			}
+			finally{
+				sr->Close();
 			}
 		}
-		catch (Exception^ e) {
-			MessageBox::Show(e->ToString());
-		}
-		finally{
-			sr->Close();
-		}
-		}
-		
+
 	}
 	private: System::Void textBox1_DragEnter(System::Object^  sender, System::Windows::Forms::DragEventArgs^  e) {
 		if (e->Data->GetDataPresent(DataFormats::FileDrop)) {
@@ -359,15 +373,15 @@ namespace ReadFile1 {
 		try {
 			sr = gcnew StreamReader(path);
 			String^ line;
-			String^ val="";
+			String^ val = "";
 
-			while ((line=sr->ReadLine()) != nullptr) {
+			while ((line = sr->ReadLine()) != nullptr) {
 				std::string str = msclr::interop::marshal_as<std::string>(line);
 				std::smatch m;
 				if (std::regex_match(str, m, std::regex("[A-Z][0-9]{2}[)]"))) {
-					std::string tmp=m[0].str();
-					String^ cpp= msclr::interop::marshal_as<System::String^>(tmp);
-					MessageBox::Show("++"+cpp);
+					std::string tmp = m[0].str();
+					String^ cpp = msclr::interop::marshal_as<System::String^>(tmp);
+					MessageBox::Show("++" + cpp);
 				}
 
 				for (System::Text::RegularExpressions::Match^ match = regex->Match(line);
@@ -375,17 +389,17 @@ namespace ReadFile1 {
 				{
 					if (match->Value->Length > 0)
 					{
-						String^ cName=match->Value;
+						String^ cName = match->Value;
 						//一致する文字列(line)があったときに"x="で始まる部分を検索し、値を取得する
 						if (regexX->Match(line)->Success) {
 							val = regexX->Match(line)->Value->ToString();
 							cmpNamelist->Add(cName);
 							cmpX->Add(val);
-							
+
 						}
 						else {
 							val = "";
-							
+
 						}
 						MessageBox::Show(match->Value + "::" + val);
 					}
@@ -408,53 +422,90 @@ namespace ReadFile1 {
 		}
 
 	}
-private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) {
-	String^ path = "C:\\Users\\chach\\Desktop\\ppt.pptx";
-	int slide_Index = 1;
-	int shapesIndex = 1;
-	Microsoft::Office::Interop::PowerPoint::Application^ apt = gcnew Microsoft::Office::Interop::PowerPoint::ApplicationClass();
-	Microsoft::Office::Interop::PowerPoint::Presentations^ presen = apt->Presentations;
-	Microsoft::Office::Interop::PowerPoint::Presentation^ presense = presen->Open(
-		path,
-		MsoTriState::msoFalse,
-		MsoTriState::msoFalse,
-		MsoTriState::msoFalse
-	);
+	private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) {
+		String^ path = "C:\\Users\\chach\\Desktop\\ppt.pptx";
+		int slide_Index = 1;
+		int shapesIndex = 1;
+		Microsoft::Office::Interop::PowerPoint::Application^ apt = gcnew Microsoft::Office::Interop::PowerPoint::ApplicationClass();
+		Microsoft::Office::Interop::PowerPoint::Presentations^ presen = apt->Presentations;
+		Microsoft::Office::Interop::PowerPoint::Presentation^ presense = presen->Open(
+			path,
+			MsoTriState::msoFalse,
+			MsoTriState::msoFalse,
+			MsoTriState::msoFalse
+		);
 
-	Microsoft::Office::Interop::PowerPoint::Shape^ shape=presense->Slides[slide_Index]->Shapes[shapesIndex];
-	
-	if (shape->TextFrame->HasText == MsoTriState::msoTrue) {
-		MessageBox::Show(shape->TextFrame->TextRange->Text);
-	}
-	else {
-		MessageBox::Show(shape->Name+"テキストはありません");
-	}
-	//以下のコードで[1,1]セルのテキストが取得できる
-	//取得した図形がテーブルであれば処理をする
-	if (shape->HasTable==MsoTriState::msoTrue) {
-		String^ text = shape->Table->Cell(1, 1)->Shape->TextFrame->TextRange->Text;
-		MessageBox::Show(text);
-	}
+		Microsoft::Office::Interop::PowerPoint::Shape^ shape = presense->Slides[slide_Index]->Shapes[shapesIndex];
 
-	presense->Close();
-}
-private: System::Void button3_Click(System::Object^  sender, System::EventArgs^  e) {
-	cli::array<String^>^ filelist=Directory::GetFiles("C:\\Users\\chach\\Desktop\\py");
-	String^ sample = "debag1";
-	for each (String^ var in filelist)
-	{
-		if (var->Contains(sample)) {
-			MessageBox::Show(var);
+		if (shape->TextFrame->HasText == MsoTriState::msoTrue) {
+			MessageBox::Show(shape->TextFrame->TextRange->Text);
+		}
+		else {
+			MessageBox::Show(shape->Name + "テキストはありません");
+		}
+		//以下のコードで[1,1]セルのテキストが取得できる
+		//取得した図形がテーブルであれば処理をする
+		if (shape->HasTable == MsoTriState::msoTrue) {
+			String^ text = shape->Table->Cell(1, 1)->Shape->TextFrame->TextRange->Text;
+			MessageBox::Show(text);
+		}
+
+		presense->Close();
+	}
+	private: System::Void button3_Click(System::Object^  sender, System::EventArgs^  e) {
+		cli::array<String^>^ filelist = Directory::GetFiles("C:\\Users\\chach\\Desktop\\py");
+		String^ sample = "debag1";
+		for each (String^ var in filelist)
+		{
+			if (var->Contains(sample)) {
+				MessageBox::Show(var);
+			}
 		}
 	}
-}
-private: System::Void button4_Click(System::Object^  sender, System::EventArgs^  e) {
-	String^ path = "C:\\Users\\chach\\Desktop\\py.zip";
-	ZipArchive^ za=ZipFile::OpenRead(path);
-	for each (ZipArchiveEntry^ val in za->Entries)
-	{
-		MessageBox::Show(val->FullName);
+	private: System::Void button4_Click(System::Object^  sender, System::EventArgs^  e) {
+		String^ path = "C:\\Users\\chach\\Desktop\\py.zip";
+		ZipArchive^ za = ZipFile::OpenRead(path);
+		for each (ZipArchiveEntry^ val in za->Entries)
+		{
+			MessageBox::Show(val->FullName);
+		}
 	}
-}
-};
-}
+	private: System::Void button5_Click(System::Object^  sender, System::EventArgs^  e) {
+		//出力先を指定
+		XmlWriter^ writer = XmlWriter::Create("C:\\Users\\chach\\Desktop\\test.xml");
+		//ルートノードを一つにしないと、エラー発生
+		writer->WriteStartElement("root");
+		//見出しを指定
+		writer->WriteStartElement("targetA");
+		//見出しから空白あけて値を指定
+		writer->WriteAttributeString("value", "data");
+		writer->WriteStartElement("targetA-1");
+		writer->WriteAttributeString("size", "0.5");
+		writer->WriteAttributeString("color", "red");
+		writer->WriteEndElement();
+		writer->WriteStartElement("targetA-2");
+		writer->WriteAttributeString("size", "7.4");
+		writer->WriteAttributeString("color", "white");
+		writer->WriteEndElement();
+		writer->WriteEndElement();
+
+		writer->WriteStartElement("targetB");
+		//見出しから空白あけて値を指定
+		writer->WriteAttributeString("value", "data");
+		writer->WriteStartElement("targetB-1");
+		writer->WriteAttributeString("size", "0.5");
+		writer->WriteAttributeString("color", "red");
+		writer->WriteEndElement();
+		writer->WriteStartElement("targetB-2");
+		writer->WriteAttributeString("size", "7.4");
+		writer->WriteAttributeString("color", "white");
+		writer->WriteEndElement();
+		writer->WriteEndElement();
+
+		writer->WriteEndElement();
+		
+		writer->Close();
+		MessageBox::Show("出力完了");
+	}
+	};
+	}
